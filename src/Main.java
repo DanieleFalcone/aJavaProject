b363206a5e2257f836115b96d2e10093c5dfea44
Daniele Falcone <Daniele.falcone@gmx.de>
2016-11-23 10:01:48 +0100

    a complex java code

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+
+import javax.swing.JFileChooser;
+
+import java.io.File;
+
+
+public class Main {
+
+	
+		public static void main(String[] args){
+		//	JFrame parentFrame = new JFrame();
+			JFileChooser fileChooser = new JFileChooser();
+			fileChooser.setDialogTitle("Speichern unter");   
+			 
+			int userSelection = fileChooser.showOpenDialog(fileChooser);
+			 
+			if (userSelection == JFileChooser.APPROVE_OPTION) {
+			    File fileToSave = fileChooser.getSelectedFile();
+			    System.out.println("Save as file: " + fileToSave.getAbsolutePath());
+			    System.out.println(fileToSave.getName());
+			}
+	    } 
+	}
+
+
