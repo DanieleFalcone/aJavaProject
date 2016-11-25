470a7b51f480f78b9a41f512d2c429109ac41b9e
Daniele Falcone <Daniele.falcone@gmx.de>
2016-11-25 14:51:34 +0100

    i added a new Feature

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,7 @@ public class Main {
 			    File fileToSave = fileChooser.getSelectedFile();
 			    System.out.println("Save as file: " + fileToSave.getAbsolutePath());
 			    System.out.println(fileToSave.getName());
+			    System.out.println("this is a new Branch with a new Feature");
 			}
 	    } 
 	}
